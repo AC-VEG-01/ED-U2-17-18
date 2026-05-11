d045738795b7a2dd75a5313adb7ee2825aec5eb4
AC-VEG-01 <L24550337@chihuahua2.tecnm.mx>
2026-05-10 19:13:16 -0600

    Add files via upload

---
+++ EVA2_7_COLLECTIONS/src/main/java/com/mycompany/eva2_7_collections/EVA2_7_COLLECTIONS.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_7_collections;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author Angel Vega
+ */
+public class EVA2_7_COLLECTIONS {
+
+    public static void main(String[] args) {
+
+       LinkedList<Integer> lista = new LinkedList<Integer>();
+    }
+}
